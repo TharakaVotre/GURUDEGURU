0e5f04c5c9d505148fe2b372de8b31455c762a13
Tharaka <Tharaka@votreit.com>
2017-02-21 11:56:22 +0530

    Initial Commit

---
+++ GDWEBSolution/GDWEBSolution/Controllers/BookController.cs
@@ -0,0 +1,117 @@
+﻿using GDWEBSolution.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GDWEBSolution.Controllers
+{
+    public class BookController : Controller
+    {
+        //
+        // GET: /Book/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Book model)
+        {
+            string name = model.Name;
+            return View();
+        }
+        //
+        // GET: /Book/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult ShowPartialView(string Bookid)
+        {
+            return PartialView("PartialView");
+        }
+
+        //
+        // GET: /Book/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Book/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Book/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Book/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Book/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Book/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
