cc62cffc1f55796028cded6cb37d8dbc6cbac0c0
Tharaka <Tharaka@votreit.com>
2017-06-20 12:12:57 +0530

    PullRequest COmmit

--- GDWEBSolution/GDWEBSolution/Models/Event/EventModel.cs
+++ GDWEBSolution/GDWEBSolution/Models/Event/EventModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-
+//Created by TM
 namespace GDWEBSolution.Models.Event
 {
     public class EventModel
